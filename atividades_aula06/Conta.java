74ccc135858d4d2f04d8b399e70bfe369b7feb82
Amanda <amandarooscosta@gmail.com>
2025-09-25 13:50:03 -0300

    Update Conta.java

--- atividades_aula06/Conta.java
+++ atividades_aula06/Conta.java
@@ -1,5 +1,13 @@
 public class Conta {
-    protected double saldo = 0.0f;
+    protected double saldo;
+
+    public Conta(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 
     public void setSaldo(double saldo) {
         this.saldo = saldo;
