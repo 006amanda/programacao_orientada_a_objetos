f0e777ea5112ce6dac9fe3c847b0e77b7a6da760
Amanda <amandarooscosta@gmail.com>
2025-09-25 14:02:56 -0300

    Create Escola.java

--- atividades_aula06/Escola.java
+++ atividades_aula06/Escola.java
@@ -1,7 +1,12 @@
 public class Escola {
     protected String nomeEscola;
 
+    public Escola() {
+    }
     public String getNomeEscola() {
         return nomeEscola;
+    }
+    public void setNomeEscola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
     }
 }
