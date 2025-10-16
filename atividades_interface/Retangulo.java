d97d2df4d310a80fd5fccf0b934687ce661561d4
Amanda <amandarooscosta@gmail.com>
2025-10-16 13:19:17 -0300

    Create Retangulo.java

---
+++ atividades_interface/Retangulo.java
@@ -0,0 +1,20 @@
+public class Retangulo extends Forma {
+    private double largura;
+    private double altura;
+
+    public Retangulo(String cor, double largura, double altura) {
+        super(cor);
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return largura * altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Retângulo " + getCor() + " -> Área: " + calcularArea();
+    }
+}
