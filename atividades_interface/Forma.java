e06aee9fef1c113001fed2b0d11a3df06a750c5e
Amanda <amandarooscosta@gmail.com>
2025-10-16 13:19:03 -0300

    Create Forma.java

---
+++ atividades_interface/Forma.java
@@ -0,0 +1,15 @@
+public abstract class Forma implements AreaCalculavel {
+    private String cor;
+
+    public Forma(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
