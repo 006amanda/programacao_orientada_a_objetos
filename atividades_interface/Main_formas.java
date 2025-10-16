92cdae28ff3efb2d2303f5bf5e324bfcbf0e1568
Amanda <amandarooscosta@gmail.com>
2025-10-16 13:20:19 -0300

    Create Main_formas.java

---
+++ atividades_interface/Main_formas.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Main_formas {
+    public static void main(String[] args) {
+        ArrayList<AreaCalculavel> formas = new ArrayList<>();
+
+        formas.add(new Retangulo("vermelho", 5, 3));
+        formas.add(new Circulo("verde", 2.5));
+
+        double somaAreas = 0;
+
+        System.out.println("ÁREAS DAS FORMAS:");
+        for (AreaCalculavel f : formas) {
+            System.out.println(f.toString());
+            somaAreas += f.calcularArea();
+        }
+
+        System.out.println("Soma total das áreas: " + somaAreas);
+    }
+}
