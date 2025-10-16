55e5b4ee6c10b45a2efb1a0e5e9c5974d1537631
Amanda <amandarooscosta@gmail.com>
2025-10-16 13:31:36 -0300

    Create Veiculo.java

---
+++ atividades_interface/Veiculo.java
@@ -0,0 +1,28 @@
+public abstract class Veiculo implements Alugavel {
+    private String placa;
+    private String modelo;
+    private int ano;
+
+    public Veiculo(String placa, String modelo, int ano) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    @Override
+    public String toString() {
+        return modelo + " -> " + placa + " -> ano: " + ano;
+    }
+}
